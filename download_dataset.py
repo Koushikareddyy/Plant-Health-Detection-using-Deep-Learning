08d29643fc17f3519b9573c157215c45376c6e1c
Koushika Reddy <2300032566@klh.edu.in>
2025-10-24 10:56:45 +0530

    Delete download_dataset.py

--- download_dataset.py
+++
@@ -1,7 +0,0 @@
-import kagglehub
-
-# Download the latest version of the dataset
-path = kagglehub.dataset_download("emmarex/plantdisease")
-
-print("✅ Dataset downloaded successfully!")
-print("📁 Path to dataset files:", path)
